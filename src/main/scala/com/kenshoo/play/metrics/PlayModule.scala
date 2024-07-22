0c87cd13dafa7c1e81fd87ea03e62b5fcf28510e
donhenryc <contacto@thchang.com>
2024-07-22 18:36:06 +0200

    commit clone ok

---
+++ src/main/scala/com/kenshoo/play/metrics/PlayModule.scala
@@ -0,0 +1,20 @@
+package com.kenshoo.play.metrics
+
+import play.api.{Configuration, Environment}
+import play.api.inject.{Binding, Module}
+
+class PlayModule extends Module {
+  override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = {
+    if (configuration.get[Boolean]("metrics.enabled")) {
+      Seq(
+        bind[MetricsFilter].to[MetricsFilterImpl].eagerly,
+        bind[Metrics].to[MetricsImpl].eagerly
+      )
+    } else {
+      Seq(
+        bind[MetricsFilter].to[DisabledMetricsFilter].eagerly,
+        bind[Metrics].to[DisabledMetrics].eagerly
+      )
+    }
+  }
+}
